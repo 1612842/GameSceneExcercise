a42214ff97a4e012a2ac89e2eb9e7a73b911476b
Cong. Le Thanh <conglt@vng.com.vn>
2019-09-25 13:54:48 +0700

    update jump

--- Assets/Scenes/Mario.cs
+++ Assets/Scenes/Mario.cs
@@ -6,10 +6,15 @@ public class Mario : MonoBehaviour
 {
     public GameObject test;
     Vector2 pos;
+    public float speed;
+    Rigidbody2D rigid2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        pos = transform.position;
+        //pos = transform.position;
+        speed = 10;
+        rigid2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,20 +24,28 @@ public class Mario : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                pos.x += 0.1f;
-                transform.position = pos;
+                //pos.x += 0.1f;
+                //transform.position = pos;
+
+                rigid2d.AddForce(new Vector2(speed, 0));
                 GetComponent<SpriteRenderer>().flipX = false;
                 GetComponent<Animator>().SetBool("Running", true);
             }
             else
             {
-                pos.x -= 0.1f;
-                transform.position = pos;
+                //pos.x -= 0.1f;
+                rigid2d.AddForce(new Vector2(-speed, 0));
+                //transform.position = pos;
                 GetComponent<SpriteRenderer>().flipX = true;
                 GetComponent<Animator>().SetBool("Running", true);
             }
         }
         else
             GetComponent<Animator>().SetBool("Running", false);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigid2d.AddForce(new Vector2(0, 50));
+        }
     }
 }
